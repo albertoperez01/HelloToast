23536569ac5144b992fba0fd7e8c47b4511e7458
Alberto Pérez Ancín <aperez@cifpfbmoll.eu>
2020-11-09 18:09:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
